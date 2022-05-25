8ab013197c6052eba7a1067d264b5ca1eeb1acf8
Alifio Rasendriya Rasyid <official.alifio@gmail.com>
2022-05-25 19:01:55 +0700

    Add cors configuration

--- src/main/java/com/binus/nvjbackend/config/WebSecurityConfig.java
+++ src/main/java/com/binus/nvjbackend/config/WebSecurityConfig.java
@@ -13,6 +13,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -71,5 +74,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**",
+        new CorsConfiguration().applyPermitDefaultValues());
+    return source;
   }
 }
